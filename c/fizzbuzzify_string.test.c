661a8b878780c3c6a3d1bc467531fde680416cfa
JimTheMan <mrdotjim@gmail.com>
2019-11-10 23:55:43 -0500

    changes to c version

---
+++ c/fizzbuzzify_string.test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+// #include "CUnit/CUnit.h"
+// #include "CUnit/Basic.h"
+
+#include "fizzbuzzify_string.test.h"
+
+void test_maxi(void)
+{
+//   CU_ASSERT(maxi(0,2) == 2);
+//   CU_ASSERT(maxi(0,-2) == 0);
+//   CU_ASSERT(maxi(2,2) == 2);
+
+    printf("Hello from testfile!\n");
+}
---
+++ c/mainy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "CUnit/CUnit.h"
+#include "CUnit/Basic.h"
+#include "fizzbuzzify_string.test.h"
+
+int main(void) {
+  printf("Hello World\n");
+
+  // maxi(1, 2);
+
+  test_maxi();
+
+  return 0;
+}
+
